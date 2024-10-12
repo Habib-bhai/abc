8ae3984ef6c463689008d7d4d75ca04bbcd35737
Habib-bhai <habibahmed918131@gmail.com>
2024-10-12 14:46:21 +0500

    obj

--- practice.js
+++ practice.js
@@ -6,16 +6,20 @@ console.log("hello git")
 
 // aljsdlfkjalsdjf
 // aljsdlfkjalsdjf
-console.log("hello 1")
-console.log("hello 2")
-console.log("hello 3")
-console.log("hello 4")
-console.log("hello 5")
-console.log("hello 6")
+// console.log("hello 1")
+// console.log("hello 2")
+// console.log("hello 3")
+// console.log("hello 4")
+// console.log("hello 5")
+// console.log("hello 6")
 
-console.log("hello")
-console.log("hello 1")
-console.log("hello 2")
-console.log("hello 3")
-console.log("hello 4")
+// console.log("hello")
+// console.log("hello 1")
+// console.log("hello 2")
+// console.log("hello 3")
+// console.log("hello 4")
+
+let obj = {
+    name: habib
+}
 
