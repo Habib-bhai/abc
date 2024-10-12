376dff2205e309e2ef2fb63d3be5ee22f7b39783
Habib-bhai <habibahmed918131@gmail.com>
2024-10-12 14:42:03 +0500

    changes

--- practice.js
+++ practice.js
@@ -2,6 +2,12 @@
 
 console.log("hello world")
 console.log("hello git")
-console.log("hello")
 
 // aljsdlfkjalsdjf
+// aljsdlfkjalsdjf
+console.log("hello 1")
+console.log("hello 2")
+console.log("hello 3")
+console.log("hello 4")
+console.log("hello 5")
+console.log("hello 6")
