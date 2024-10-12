94362373c88d422f5f9ab1853b6a79942f44e335
Habib-bhai <habibahmed918131@gmail.com>
2024-10-12 14:47:11 +0500

    array

--- practice.js
+++ practice.js
@@ -1,10 +1,11 @@
 
 
-console.log("hello world")
-console.log("hello git")
-console.log("hello")
-console.log("hello 1")
-console.log("hello 2")
-console.log("hello 3")
-console.log("hello 4")
+// console.log("hello world")
+// console.log("hello git")
+// console.log("hello")
+// console.log("hello 1")
+// console.log("hello 2")
+// console.log("hello 3")
+// console.log("hello 4")
 
+let arr = [1,2,3,4]
